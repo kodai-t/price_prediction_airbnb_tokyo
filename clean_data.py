6baaa6bab350ce83f393db3a59c1397907d22a4e
Kodai Takigawa <kodaitakigawa@Kodais-MacBook-Pro.local>
2019-10-08 19:54:25 -0600

    Let's analyze.(Should I choose depending on correlation?)

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/listings.csv', low_memory=False)
 
@@ -14,7 +15,10 @@ drop_columns = ['id', 'listing_url','scrape_id', 'last_scraped', 'name', 'summar
                 'calendar_last_scraped', 'license', 'jurisdiction_names', 'zipcode', 'first_review', 'last_review',
                 'requires_license', 'require_guest_profile_picture', 'require_guest_phone_verification',
                 # later I should adjust these below columns to int
-                'host_since', 'host_response_time', 'host_response_rate']
+                'host_since', 'host_response_time', 'host_response_rate', 'host_neighbourhood', 'host_verifications',
+                'city', 'amenities', 'calendar_updated', 'cancellation_policy',
+                # Remove some data based on correlation
+                'host_total_listings_count']
 reduced_columns_listings = df.drop(drop_columns, axis=1)
 
 # Extract specific areas
@@ -28,25 +32,47 @@ specific_areas_listings = reduced_columns_listings[(reduced_columns_listings['ne
 specific_areas_listings = specific_areas_listings.dropna(axis=0, how='any')
 
 # Change char to int in 'price'
-processed_listings = specific_areas_listings.replace({'price': {'\$': ''}})
+processed_listings = specific_areas_listings.replace({'price': {'\$': '', ',': '', '.00': ''},
+                                                      'security_deposit': {'\$': '', ',': '', '.00': ''},
+                                                      'cleaning_fee': {'\$': '', ',': '', '.00': ''},
+                                                      'extra_people': {'\$': '', ',': '', '.00': ''}
+                                                      }, regex=True)
 
 # Mapping
-cancellation_policy_map = {'modelate': 0, 'strict_14_with_grace_period': 1}
-processed_listings['cancellation_policy'].map(cancellation_policy_map)
+# cancellation_policy_map = {'modelate': 0, 'strict_14_with_grace_period': 1}
+# processed_listings['cancellation_policy'].map(cancellation_policy_map)
 
 bool_map = {'f': 0, 't': 1}
-processed_listings['instant_bookable'].map(bool_map)
-# processed_listings['is_business_travel_ready'].map(bool_map)
-# processed_listings['host_is_superhost'].map(bool_map)
-# processed_listings['host_has_profile_pic'].map(bool_map)
-# processed_listings['host_identity_verified'].map(bool_map)
-# processed_listings['is_location_exact'].map(bool_map)
-# processed_listings['has_availability'].map(bool_map)
+processed_listings['instant_bookable'].replace(bool_map, inplace=True)
+processed_listings['is_business_travel_ready'].replace(bool_map, inplace=True)
+processed_listings['host_has_profile_pic'].replace(bool_map, inplace=True)
+processed_listings['host_identity_verified'].replace(bool_map, inplace=True)
+processed_listings['host_is_superhost'].replace(bool_map, inplace=True)
+processed_listings['is_location_exact'].replace(bool_map, inplace=True)
+processed_listings['has_availability'].replace(bool_map, inplace=True)
 
-print(processed_listings['instant_bookable'])
-quit()
-print(processed_listings.where(processed_listings == 't'))
+area_map = {'Shinjuku Ku': 0, 'Taito Ku': 1, 'Toshima Ku': 2, 'Shibuya Ku': 3, 'Minato Ku': 4}
+processed_listings['neighbourhood_cleansed'].replace(area_map, inplace=True)
 
+property_n_room_type_map = {'Apartment': 0, 'House': 1, 'Serviced apartment': 2, 'Condominium': 3, 'Villa': 4, 'Hostel': 5,
+                     'Aparthotel': 6, 'Ryokan (Japan)': 7, 'Hotel': 8, 'Boutique hotel': 9, 'Guest suite': 10,
+                     'Guesthouse': 11, 'Cabin': 12, 'Other': 13, 'Townhouse': 14, 'Loft': 15, 'Dorm': 16,
+                     'Tiny house': 17, 'Bungalow': 18, 'Hut': 19, 'Camper/RV': 20, 'Bed and breakfast': 21,
+                     'Entire home/apt': 22, 'Private room': 23, 'Shared room': 24}
+processed_listings['property_type'].replace(property_n_room_type_map, inplace=True)
+processed_listings['room_type'].replace(property_n_room_type_map, inplace=True)
+
+bed_type_map = {'Real Bed': 0, 'Futon': 1, 'Pull-out Sofa': 2}
+processed_listings['bed_type'].replace(bed_type_map, inplace=True)
+
+# Make training set and test set
+processed_listings = processed_listings.apply(pd.to_numeric)
 train_set, test_set = train_test_split(processed_listings, test_size=0.25)
-sns.heatmap(train_set.astype(float))
+
+# Visualize correlation by using heatmap
+# colormap = plt.cm.RdBu
+# heat_map = sns.heatmap(train_set.corr(),linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white')
+# plt.show()
+
+print(train_set.corr()['price'])
 
